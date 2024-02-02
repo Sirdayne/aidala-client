0b697d45d283470bfc747ce5729b65c51216cb1a
Yernar <yernar.mussin@nitka.com>
2024-02-03 00:17:40 +0600

    first section

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
-import Navigation from './ui/Navigation.vue';
 </script>
 
 <template>
   <div>
-    <Navigation></Navigation>
     <router-view></router-view>
   </div>
 </template>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -2,5 +2,25 @@
 </script>
 
 <template>
-  <h1>Home</h1>
+  <div class="home">
+    <div class="home-gradient">
+      <div class="home-gradient__block"></div>
+    </div>
+
+    <div class="home-logo">
+      <img src="../..//assets/img/logo_aidala.svg" alt="">
+    </div>
+    <h1 class="home-title">Welcome to <span class="home-title__name">AiDala</span></h1>
+    <div class="home-description__container">
+      <h2 class="home-description">
+        "Revolutionising" is often overused. "exclusive features" is vague. Suggest alternative copy: Refine Your Market Edge.
+        With custom AI analytics, navigate through market noise and pinpoint risk with precision. Our platform is your
+        ally—equipping you with intelligent tools tailored for strategic decision-making. Elevate your trades, join the ranks of elite traders
+      </h2>
+    </div>
+
+    <div class="home-benefits">
+      <h2 class="home-benefits-title">Our key benefits</h2>
+    </div>
+  </div>
 </template>
